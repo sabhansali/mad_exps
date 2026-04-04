5786ee0615f158b127a54ba3cb1b2edc60615d4e
sabhansali <sbhansali444@gmail.com>
2026-04-04 14:59:00 +0530

    exp 02

---
+++ View_Viewgroups_D12A07/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "View_Viewgroups_D12A07"
+include(":app")
+ 
